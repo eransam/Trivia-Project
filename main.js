7d72c971c5b0a1c52542599e21a4d0024abb239c
eransam <eransam21@gmail.com>
2022-05-31 16:58:44 +0300

    first commit

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 
+const eran123 = 1;
+const elements2 = document.getElementsByName('button');
+
+
 function myFunction(event) { 
     var answer = event.target.innerHTML;
-    if(answer!= "jerusalem")
+    if(answer == testo)
     {
-        event.currentTarget.className = "redBackground";
-
+        event.currentTarget.className = "greenBackground";
          //event.target.style.backgroundColor="#FF0000";
     }
     else
     {         
-        event.currentTarget.className = "greenBackground";
+        event.currentTarget.className = "redBackground";
     }
-    checkboxes = document.getElementsByName('button');
-    console.log("checkboxes: ",checkboxes[0]);
-    for (let index = 0; index < checkboxes.length; index++) {
-        checkboxes[index].disabled = true;
-  }
-    document.getElementById("demo").innerHTML = "Triggered by a " + answer + " element";
+
+ elements2.disabled = true;
+
+
+console.log("testo" , testo);
+
+
   }
 
 
@@ -30,17 +34,31 @@ function myFunction(event) {
    // }
 
 
-   async function showtrivia() {
+   async function showtrivia(event) {
     try {
-      const trivia = await getUsersAsync();
+
+        event.target.innerHTML = "לשאלה הבאה";
+        console.log("erer: " , eran123);
+      const trivia = await getTriviaAsync();
       console.log("trivia: " , trivia);
-      displayQastTable(trivia.results);
+      const qus = trivia.results;
+      console.log("qus[0]: ",qus[0]);
+      const i =0;
+      qusEeth = qus[i];
+      displayQastTable(qusEeth);
         } catch (error) {
       alert("Error getting users. Error: " + error);
+
+
     }
   }
+
+  function nextQ() {
+    eran123 = eran123+1; 
+    showtrivia();
+  }
   
-  function getUsersAsync() {
+  function getTriviaAsync() {
     return new Promise((resolve, reject) => {
       const ajax = new XMLHttpRequest();
       ajax.onreadystatechange = () => {
@@ -63,43 +81,42 @@ function myFunction(event) {
   function onLoad() {
     theTemp = document.getElementById("qast");
   }
-
+  var testo = "";
   function displayQastTable(qastArray) {
     theTemp.innerHTML = "";
   
-    let contentHTML = "eran";
-    console.log("qastArray[0]: " ,qastArray[0]);
+    let contentHTML = "";
+    testo = qastArray.correct_answer;
+    console.log("testo: " , testo);
+    //console.log("qastArray[0]: " ,qastArray[0]);
 
     
-  
-    for (const prop of qastArray) {
-     // contentHTML += `<h3>${prop.question}</h3>`;
-    //}
+
     contentHTML += `
-    <h3>${prop.question}</h3>
-    <button id="${prop.correct_answer}" name="button" class="inlineBlock" onclick="myFunction(event)">${prop.correct_answer}</button></br>
+    <h3>${qastArray.question}</h3></br>
+    <button id="${qastArray.correct_answer}" name="button" class="" onclick="myFunction(event)">${qastArray.correct_answer}</button></br>
     `;
 
-      for (const prop of qastArray) {
-    }
-              console.log("prop2: " , prop.incorrect_answers);
+     // for (const prop of qastArray) {
+    //}
+         //     console.log("prop2: " , prop.incorrect_answers);
 
-        for (let index = 0; index < prop.incorrect_answers.length; index++) {
-            const element = prop.incorrect_answers[index];
+        for (let index = 0; index < qastArray.incorrect_answers.length; index++) {
+            const element = qastArray.incorrect_answers[index];
             console.log("element: " ,element);
-            contentHTML += `<button id="${element}" name="button" class="inlineBlock" onclick="myFunction(event)">${element}</button></br>`;
+            contentHTML += `<button id="${element}" name="button" class="" onclick="myFunction(event)">${element}</button></br>`;
 
         }
 
-              console.log("correct_answer: " , prop.correct_answer);
+            //  console.log("correct_answer: " , prop.correct_answer);
 
 
+            theTemp.innerHTML = contentHTML;
 
         }
 
   
-    theTemp.innerHTML = contentHTML;
-  }
+  
 
 
 
