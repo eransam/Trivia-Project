b735b9903d1f6545ffa266d3b25d2c36fa1f5863
eransam <eransam21@gmail.com>
2022-05-31 22:27:55 +0300

    first commit

--- main.js
+++ main.js
@@ -1,125 +1,94 @@
+var elements2 = document.getElementsByName("button");
+var testo = "";
+var i = 0;
+var theTemp = "";
+var contentHTML = "";
+var trivia = "";
 
-const eran123 = 1;
-const elements2 = document.getElementsByName('button');
+function nextQ() {
+  const test = document.querySelector("button");
+  console.log("test: ", test);
+  const elements2 = document.getElementsByName("button");
+  console.log("elements2: ", elements2);
+  for (let index = 0; index < elements2.length; index++) {
+    const element = elements2[index];
+    console.log("element inside: ", element);
+    element.disabled = true;
+  }
+}
 
+function myFunction(event) {
+  var answer = event.target.innerHTML;
+  if (answer == testo) {
+    event.currentTarget.className = "greenBackground";
+  } else {
+    event.currentTarget.className = "redBackground";
+  }
+  elements2.disabled = true;
+  console.log("testo", testo);
 
-function myFunction(event) { 
-    var answer = event.target.innerHTML;
-    if(answer == testo)
-    {
-        event.currentTarget.className = "greenBackground";
-         //event.target.style.backgroundColor="#FF0000";
+  elements2 = document.getElementsByName("button");
+  for (let index = 0; index < elements2.length; index++) {
+    const element = elements2[index];
+    if ((element.id = answer)) {
+      element.disabled = false;
     }
-    else
-    {         
-        event.currentTarget.className = "redBackground";
-    }
-
- elements2.disabled = true;
-
-
-console.log("testo" , testo);
-
-
+    element.disabled = true;
   }
+}
 
+async function onLoad() {
+  trivia = await getTriviaAsync();
+}
 
-  //function lock() { 
-   // checkboxes = document.getElementsByName('button');
-   // console.log("checkboxes: ",checkboxes[0]);
-    //for (let index = 0; index < checkboxes.length; index++) {
-       // checkboxes[index].disabled = true;
-  //}
-        
-   // }
-
-
-   async function showtrivia(event) {
-    try {
-
-        event.target.innerHTML = "לשאלה הבאה";
-        console.log("erer: " , eran123);
-      const trivia = await getTriviaAsync();
-      console.log("trivia: " , trivia);
-      const qus = trivia.results;
-      console.log("qus[0]: ",qus[0]);
-      const i =0;
-      qusEeth = qus[i];
-      displayQastTable(qusEeth);
-        } catch (error) {
-      alert("Error getting users. Error: " + error);
-
-
-    }
-  }
-
-  function nextQ() {
-    eran123 = eran123+1; 
-    showtrivia();
-  }
-  
-  function getTriviaAsync() {
-    return new Promise((resolve, reject) => {
-      const ajax = new XMLHttpRequest();
-      ajax.onreadystatechange = () => {
-        if (ajax.readyState === 4) {
-          if (ajax.status === 200) {
-            const tri = JSON.parse(ajax.responseText);
-            resolve(tri);
-          }
-          else {
-            reject(ajax.status);
-          }
-        }
-      };
-  
-      ajax.open("GET", "https://opentdb.com/api.php?amount=10");
-      ajax.send();
-    });
-  }
-
-  function onLoad() {
+//פונ' ראשית בכפתור
+async function showtrivia(event) {
+  try {
     theTemp = document.getElementById("qast");
+    event.target.innerHTML = "לשאלה הבאה";
+    //מושך את השאלות
+    const TheArrayQuestions = trivia.results;
+    var oneQuestions = TheArrayQuestions[i];
+    displayQastTable(oneQuestions);
+    i = i + 1;
+  } catch (error) {
+    alert("Error getting users. Error: " + error);
   }
-  var testo = "";
-  function displayQastTable(qastArray) {
-    theTemp.innerHTML = "";
-  
-    let contentHTML = "";
-    testo = qastArray.correct_answer;
-    console.log("testo: " , testo);
-    //console.log("qastArray[0]: " ,qastArray[0]);
+}
 
-    
-
-    contentHTML += `
-    <h3>${qastArray.question}</h3></br>
-    <button id="${qastArray.correct_answer}" name="button" class="" onclick="myFunction(event)">${qastArray.correct_answer}</button></br>
-    `;
-
-     // for (const prop of qastArray) {
-    //}
-         //     console.log("prop2: " , prop.incorrect_answers);
-
-        for (let index = 0; index < qastArray.incorrect_answers.length; index++) {
-            const element = qastArray.incorrect_answers[index];
-            console.log("element: " ,element);
-            contentHTML += `<button id="${element}" name="button" class="" onclick="myFunction(event)">${element}</button></br>`;
-
+//מושך את השאלות
+function getTriviaAsync() {
+  return new Promise((resolve, reject) => {
+    const ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = () => {
+      if (ajax.readyState === 4) {
+        if (ajax.status === 200) {
+          const tri = JSON.parse(ajax.responseText);
+          resolve(tri);
+        } else {
+          reject(ajax.status);
         }
+      }
+    };
 
-            //  console.log("correct_answer: " , prop.correct_answer);
+    ajax.open("GET", "https://opentdb.com/api.php?amount=10");
+    ajax.send();
+  });
+}
 
+//מקבל כל פעם שאלה אחת ומציג אותה
+function displayQastTable(qastArray) {
+  theTemp.innerHTML = "";
+  contentHTML = "";
+  testo = qastArray.correct_answer;
+  contentHTML += `
+            <h3>${qastArray.question}</h3></br>
 
-            theTemp.innerHTML = contentHTML;
+            <button id="${qastArray.correct_answer}" name="button" class="" onclick="myFunction(event)">${qastArray.correct_answer}</button></br>`;
 
-        }
-
-  
-  
-
-
-
-
-  //correct_answer
-  //incorrect_answers
+  for (let index = 0; index < qastArray.incorrect_answers.length; index++) {
+    const element = qastArray.incorrect_answers[index];
+    contentHTML += `<button id="${element}" name="button" class="" onclick="myFunction(event)">${element}</button></br>`;
+  }
+  theTemp.innerHTML = contentHTML;
+}
